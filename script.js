cef5031214114bbd291ad415506d7c578fd49634
MIKNSJ <sculetoncode@gmail.com>
2024-08-07 14:59:22 -0700

    updated header: modifications to scrolling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+var enableStickyHeader = false;
+var hideHeader = false;
 var scrollPosY = 0;
-var cooldown = 10;
 
 function clickNavBar() {
     document.getElementById("nav_bar_options").classList.toggle("show_nav_bar_options");
@@ -25,33 +26,44 @@ window.onscroll = function() {
     var header = document.getElementsByTagName("header")[0];
     var nav_bar_options = document.getElementById("nav_bar_options");
 
-    if (Math.abs(window.scrollY - scrollPosY > cooldown)) {
-        if (scrollPosY > window.scrollY) {
-            if (header.classList.contains("hide_header")) {
-                header.classList.remove("hide_header");
-            }
-
-            if (nav_bar_options.classList.contains("hide_nav_bar_options")) {
-                nav_bar_options.classList.remove("hide_nav_bar_options");
-            }
-            /* console.log("scroll-up"); */
-        } else {
-            header.classList.toggle("hide_header");
-            nav_bar_options.classList.toggle("hide_nav_bar_options");
-            /* console.log("scroll-down"); */
+    if (window.scrollY > 75) {
+        if (!enableStickyHeader) {
+            header.classList.toggle("enable_sticky_header");
+            console.log("The header has now disappeared.");
+            enableStickyHeader = true;
+            nav_bar_options.style.backgroundColor = "#FAF9F6";
         }
+
+        if (window.scrollY > 500) {
+            if (window.scrollY < scrollPosY) {
+                if (hideHeader) {
+                    if (header.classList.contains("hide_header")) {
+                        header.classList.remove("hide_header");
+                    }
+
+                    if (nav_bar_options.classList.contains("hide_nav_bar_options")) {
+                        nav_bar_options.classList.remove("hide_nav_bar_options");
+                    }
+                }
+
+                hideHeader = false;
+                /* console.log("scroll-up"); */
+            } else {
+                if (!hideHeader) {
+                    header.classList.toggle("hide_header");
+                    nav_bar_options.classList.toggle("hide_nav_bar_options");
+                }
+                hideHeader = true;
+                /* console.log("scroll-down"); */
+            }
+        }
+    } else {
+        if (header.classList.contains("enable_sticky_header")) {
+            header.classList.remove("enable_sticky_header");
+        }
+        enableStickyHeader = false;
+        nav_bar_options.style.backgroundColor = "white";
     }
 
     scrollPosY = window.scrollY;
-
-    if (scrollPosY == 0) {
-        if (header.classList.contains("hide_header")) {
-            header.classList.remove("hide_header");
-        }
-
-        if (nav_bar_options.classList.contains("hide_nav_bar_options")) {
-            nav_bar_options.classList.remove("hide_nav_bar_options");
-        }
-        /* console.log("Back at the top of the page."); */
-    }
 }
