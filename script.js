16e4d640b78153a36129e6d2a5896169b3c31353
MIKNSJ <sculetoncode@gmail.com>
2024-08-06 23:23:36 -0700

    updated header: dropdown

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-/* Nothing to see here at the moment. */
+function clickNavBar() {
+    document.getElementById("nav_bar_options").classList.toggle("show_nav_bar_options");
+    document.getElementById("nav_button_icon").classList.toggle("change_nav_button_icon");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches(".nav__button")) {
+        var nav_bar = document.getElementById("nav_bar_options");
+        var nav_button = document.getElementById("nav_button_icon")
+
+        if (nav_bar.classList.contains("show_nav_bar_options")) {
+            nav_bar.classList.remove("show_nav_bar_options");
+        }
+
+        if (nav_button.classList.contains("change_nav_button_icon")) {
+            nav_button.classList.remove("change_nav_button_icon");
+        }
+    }
+}
